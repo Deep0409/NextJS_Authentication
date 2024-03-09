33892ca4ccb39bdcd5c9726be50a32f2227fe298
Deepanshu Pariyani <pariyanideepanshu@gmail.com>
2024-03-09 16:17:25 +0530

    css changed

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -8,11 +8,11 @@ connect();
 
 export async function POST(request : NextRequest){
     try {
-        const reqBody=await request.json();
-        const {username,email,password}=reqBody;
+        const reqBody= await request.json();
+        const {username,email,password} = reqBody;
         console.log(reqBody);
 
-        const user=await User.findOne({email});
+        const user = await User.findOne({email});
 
           //check if user exists
         if(!user){
--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -8,7 +8,7 @@ export async function GET(){
             success:true
         });
 
-        response.cookies.set("tokens","",{httpOnly:true,expires:new Date(0)});
+        response.cookies.set("token","",{httpOnly:true,expires:new Date(0)});
 
         return response;
         
